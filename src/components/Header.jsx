e7f3e33810866c879df7f2e5cfe34ea9488989cb
Yaqub Muflih Wiraatmaja <yaqubmw2@gmail.com>
2024-04-13 19:58:41 +0700

    tailwindcss: remove typography and form plugins

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="prose">
+        <h1 className="flex flex-wrap font-bold text-sm sm:text-xl"><span>YMW</span><span>Estate</span></h1>
+      </div>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html", "./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [],
 };
