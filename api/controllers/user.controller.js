09fdc727f9379a7f96281c9d22fcf65ed423cfc5
Yaqub Muflih Wiraatmaja <yaqubmw2@gmail.com>
2024-04-15 10:06:03 +0700

    create: api route signup

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 export const test = (req, res) => {
   res.json({
-    message: "Hello world!!",
+    message: "API route is working!!",
   });
 };
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
-    usename: {
+    username: {
       type: String,
       required: true,
       unique: true,
